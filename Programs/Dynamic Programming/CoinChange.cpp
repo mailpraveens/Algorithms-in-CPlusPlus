ca6439becc3ec3c77de2e72c019369aa7a180240
Praveen S <mailpraveens@gmail.com>
2014-08-14 01:09:15 +0530

    Partial commits to DP problems.

--- Programs/Dynamic Programming/CoinChange.cpp
+++ Programs/Dynamic Programming/CoinChange.cpp
@@ -7,3 +7,25 @@
 //
 
 #include "CoinChange.h"
+
+
+int coinChangeProblem(int * denomination, int n, int changeToMake){
+    int M[n+1];
+    M[0] = 0;
+    // M[i] gives min coins to make change i from given values
+    for (int i = 0; i < changeToMake; i++) {
+        M[i] = INT_MAX;
+        int temp = INT_MAX;
+        for (int j = 0; j < n; j++) {
+            if (denomination[j] <=  changeToMake) {
+                //Use this coin
+                int temp_amt = denomination[i - denomination[j]] + 1;
+                if (temp_amt < temp ) {
+                    
+                }
+            }
+        }
+    }
+    
+    return M[n];
+}
--- Programs/TopCoderProblems/TheLuckyString.cpp
+++ Programs/TopCoderProblems/TheLuckyString.cpp
@@ -1,13 +1,66 @@
-//
-//  TheLuckyString.cpp
-//  TestApps
-//
-//  Created by Praveen S on 8/8/14.
-//  Copyright (c) 2014 Praveen S. All rights reserved.
-//
+// A recursive program to print all possible partitions of a given string
+// into dictionary words
+#include <iostream>
+using namespace std;
 
-#include "TheLuckyString.h"
+/* A utility function to check whether a word is present in dictionary or not.
+ An array of strings is used for dictionary.  Using array of strings for
+ dictionary is definitely not a good idea. We have used for simplicity of
+ the program*/
+int dictionaryContains(string word)
+{
+    string dictionary[] = {"mobile","samsung","sam","sung","man","mango",
+        "icecream","and","go","i","love","ice","cream"};
+    int size = sizeof(dictionary)/sizeof(dictionary[0]);
+    for (int i = 0; i < size; i++)
+        if (dictionary[i].compare(word) == 0)
+            return true;
+    return false;
+}
 
-void findPermutations(char * s) {
+//prototype of wordBreakUtil
+void wordBreakUtil(string str, int size, string result);
+
+void wordBreak(string str)
+{
+    //last argument is prefix
+    wordBreakUtil(str, str.size(), "");
+}
+
+// result store the current prefix with spaces between words
+void wordBreakUtil(string str, int size, string result)
+{
+    //Process all prefixes one by one
+    for (int i=1; i<=size; i++)
+    {
+        //extract substring from 0 to i in prefix
+        string prefix = str.substr(0, i);
+        
+        // if dictionary conatins this prefix, then we check
+        // for remaining string. Otherwise we ignore this prefix
+        // (there is no else for this if) and try next
+        if (dictionaryContains(prefix))
+        {
+            // if no more elements are there, print it
+            if (i == size)
+            {
+                // add this element to previous prefix
+                result += prefix;
+                cout << result << endl; //print result
+                return;
+            }
+            cout << " Calling function:  " << str.substr(i, size-i) << " Substring is:" <<str.substr(0, i) << endl;
+            wordBreakUtil(str.substr(i, size-i), size-i, result+prefix+" ");
+        }
+    }      //end for
+}//end function
+
+int main()
+{
+    cout << "First Test:\n";
+    wordBreak("iloveicecreamandmango");
     
+    cout << "\n\nSecond Test:\n";
+    wordBreak("ilovesamsungmobile");
+    return 0;
 }
